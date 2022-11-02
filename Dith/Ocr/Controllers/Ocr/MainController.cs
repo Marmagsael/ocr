d373ff7a59d3da4ee072999249cea848cde5e641
judithlorrenreyes <judithlorrenreyes@gmail.com>
2022-11-02 11:47:21 +0800

    "Test"

---
+++ Dith/Ocr/Controllers/Ocr/MainController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ocr.Controllers.Ocr
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Suppliers()
+        {
+            return View("~/Views/Ocr/Main/Suppliers.cshtml");
+        }
+
+        public IActionResult Customers()
+        {
+            return View("~/Views/Ocr/Main/Customers.cshtml");
+        }
+
+        public IActionResult Categories()
+        {
+            return View("~/Views/Ocr/Main/Categories.cshtml");
+
+        }
+
+        public IActionResult Projects()
+        {
+            return View("~/Views/Ocr/Main/Projects.cshtml");
+        }
+
+        public IActionResult BankAccounts()
+        {
+            return View("~/Views/Ocr/Main/BankAccounts.cshtml");
+        }
+
+        public IActionResult PaymentMethods()
+        {
+            return View("~/Views/Ocr/Main/PaymentMethods.cshtml");
+        }
+    }
+}
